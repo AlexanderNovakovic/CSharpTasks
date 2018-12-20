2cc1d83714179ae72be085e3a0dc7abdc9b00466
AlexanderStorM <salerion1@gmail.com>
2018-12-20 15:55:17 +0100

    Add Task2_9 with unit tests.

---
+++ Task2_9.UnitTests/Task2_9Tests.cs
@@ -0,0 +1,43 @@
+﻿using static System.Math;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using static Task2_9.MathExtensions;
+
+namespace Task2_9.UnitTests
+{
+    public class Task2_9Tests
+    {
+        [Fact]
+        public void SumAscendingTests() =>        
+            Assert.Equal(1.46361, Round(SumAscending(5), 5));
+
+        [Theory]
+        [InlineData(100, 1.634984)]
+        [InlineData(200, 1.639947)]
+        [InlineData(500, 1.642936)]
+        [InlineData(1000, 1.643935)]
+        [InlineData(2000, 1.644434)]
+        [InlineData(5000, 1.644734)]
+        [InlineData(10000, 1.644834)]
+        public void SumAscendingTestsTwo(int n, double expected) =>
+            Assert.Equal(expected, Round(SumAscending(n), 6));
+
+        [Fact]
+        public void SumDescendingTests() =>
+            Assert.Equal(1.46361, Round(SumDescending(5), 5));
+
+        [Theory]
+        [InlineData(100, 1.634984)]
+        [InlineData(200, 1.639947)]
+        [InlineData(500, 1.642936)]
+        [InlineData(1000, 1.643935)]
+        [InlineData(2000, 1.644434)]
+        [InlineData(5000, 1.644734)]
+        [InlineData(10000, 1.644834)]
+        public void SumDescendingTestsTwo(int n, double expected) =>
+            Assert.Equal(expected, Round(SumDescending(n), 6));
+    }
+}
---
+++ Task2_9/MathExtensions.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using static System.Math;
+
+namespace Task2_9
+{
+    public static class MathExtensions
+    {
+        public static double SumAscending(int n)
+        {
+            if (n <= 0)
+            { 
+                throw new ArgumentOutOfRangeException(nameof(n));
+            }
+
+            double sum = 0;
+
+            for (int i = 1; i <= n; i++)
+            {
+                sum += 1.0 / Pow(i, 2);
+            }
+
+            return sum;
+        }
+
+        public static double SumDescending(int n)
+        {
+            if (n <= 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(n));
+            }
+
+            double sum = 0;
+
+            for (int i = n; i > 0; i--)
+            {
+                sum += 1.0 / Pow(i, 2);
+            }
+
+            return sum;    
+        }
+    }
+}
